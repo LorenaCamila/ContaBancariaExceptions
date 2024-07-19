39681e4e96d1b87685cbf586f35c8e6fc2dbad69
LorenaCamila <lorenacamila19@gmail.com>
2024-07-19 16:15:57 -0300

    Update

---
+++ Secao11Exercicio/Entities/Exception/DomainException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Secao11Exercicio.Entities.Exception
+{
+    internal class DomainException : ApplicationException  
+    {
+        public DomainException(string message) : base(message) 
+        {
+        }
+    }
+}
---
+++ Secao11Exercicio/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Globalization;
+using Secao11Exercicio.Entities.Exception;
+using Secao11Exercicio.Entities;
+
+namespace Secao11Exercicio
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account date");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account account = new Account(number, holder, initialBalance, withdrawLimit);
+
+            Console.Write("\nEnter amount for withdraw: ");
+            double withdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            try
+            {
+                account.Withdraw(withdraw);
+                Console.WriteLine($"New Balance = {account.Balance.ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+        }
+    }
+}
